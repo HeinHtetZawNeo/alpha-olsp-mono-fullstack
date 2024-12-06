6f070cb5d5249014bdb78bc01d72e2d4d903468a
HeinHtetZawNeo <heinhtet.zaw.career@gmail.com>
2024-12-05 19:05:40 -0600

    add log to debug

--- src/main/java/com/alpha/olsp/AlphaOnlineSystemApplication.java
+++ src/main/java/com/alpha/olsp/AlphaOnlineSystemApplication.java
@@ -3,16 +3,29 @@ package com.alpha.olsp;
 import com.alpha.olsp.model.State;
 import com.alpha.olsp.repository.StateRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class AlphaOnlineSystemApplication {
     private final StateRepository stateRepository;
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @PostConstruct
+    public void logEnvVariables() {
+        System.out.println("Database URL from Environment (Spring): " + datasourceUrl);
+        System.out.println("jwtSecret URL from Environment (Spring): " + jwtSecret);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AlphaOnlineSystemApplication.class, args);
